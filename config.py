801b0d4a302ffee26e8bd287295dddcf8eebdafa
Chong Xin Yi <ellyc1130@gmail.com>
2023-09-11 21:53:28 +0800

    Setup MariaDB

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "database-1.cizar3lczvxa.us-east-1.rds.amazonaws.com"
+customhost = "student.cs98yicshm5s.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 studentdb = "student"
